9a294af3a1876b6342c342a73f1f791487a04609
MuskanAgarwal7 <160391188+MuskanAgarwal7@users.noreply.github.com>
2024-03-05 17:43:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const search = document.querySelector(".search")
+const input = document.querySelector("input")
+const notfound = document.querySelector(".notfound");
+const cityName = document.querySelector(".cityName");
+const date = document.querySelector(".Date");
+const region = document.querySelector(".State");
+const country = document.querySelector(".country");
+const temperature = document.querySelector(".temp");
+const condition = document.querySelector(".condition");
+
+(async () => {
+    let URL = 'https://api.weatherapi.com/v1/current.json?key=4b21ade6922b44ce80791051242102&q=jhansi&aqi=no'
+    let response = await fetch(URL);
+    let data = await response.json();
+    cityName.innerText = data.location.name;
+    date.innerText = data.location.localtime;
+    region.innerText = data.location.region;
+    country.innerText = data.location.country;
+    temperature.innerText = data.current.temp_c;
+    condition.innerText = data.current.condition.text;
+})();
+
+
+async function weatherapi(city) {
+    let url = `https://api.weatherapi.com/v1/current.json?key=4b21ade6922b44ce80791051242102&q=${city}&aqi=no`
+    let response = await fetch(url);
+    let data = await response.json();
+    cityName.innerText = data.location.name;
+    date.innerText = data.location.localtime;
+    region.innerText = data.location.region;
+    country.innerText = data.location.country;
+    temperature.innerText = data.current.temp_c;
+    condition.innerText = data.current.condition.text;
+}
+
+search.addEventListener("click", () => {
+    weatherapi(input.value);
+});
